869623f1e6d00481b7d395d08468a2ea5f98325d
Andrew Partis <andrew.partis@tapirs.co.uk>
2019-11-12 19:04:30 +0000

    removed extra routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,6 @@
 const contactRoutes = require('./contact_routes');
-const homelessRoutes = require('./homeless_routes');
-const lgbtqRoutes = require('./lgbtq_routes');
 
 module.exports = function(app, db, cors) {
   contactRoutes(app, db, cors);
-  homelessRoutes(app, db, cors);
-  lgbtqRoutes(app, db, cors);
   // Other route groups could go here, in the future
 };
